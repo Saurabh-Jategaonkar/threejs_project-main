1b8192bfbfd3fbe3fa04cf9a1b293c61e2e6bf12
Saurabh-Jategaonkar <sj749@njit.edu>
2023-08-26 09:41:55 -0400

    deployment_open_api_issue_1

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -18,16 +18,16 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        // const response = await openai.images.generate({
-        //     prompt,
-        //     n: 1,
-        //     size: '1024x1024',
-        //     response_format: 'b64_json'
-        // });
+        const response = await openai.images.generate({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
 
-        // const image = response.data.data[0].b64_json;
+        const image = response.data.data[0].b64_json;
 
-        // res.status(200).json({ photo: image});
+        res.status(200).json({ photo: image});
         
     } catch (error) {
         console.log(error);
